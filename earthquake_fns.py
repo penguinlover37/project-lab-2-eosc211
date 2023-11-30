48c6b9120f121649dc52aa885f8fba98fd9dc7b3
penguinlover37 <150316172+penguinlover37@users.noreply.github.com>
2023-11-29 21:52:30 -0800

    fixed func2 and made correct output

--- earthquake_fns.py
+++ earthquake_fns.py
@@ -1,63 +1,3 @@
-# import numpy as np
-# import pandas as pd
-# from datetime import datetime
-
-# #Function 1
-# def get_coastlines(coasts_file):
-#     try:
-#         df = pd.read_csv(coasts_file)
-#         lon_coast = df.iloc[:,0]
-#         lat_coast = df.iloc[:,1]
-#     except:
-#         raise IOError
-#     return lon_coast, lat_coast
-
-# #Function 2
-# def get_plate_boundaries(boundaries_file):
-#     try:
-#         df = pd.read_csv(boundaries_file)
-#         plate_dict = {}
-
-        
-#         for index, row in df.iterrows():
-#             #
-#             plate_name = row['plate']
-#             latitude = row['lat']
-#             longitude = row['lon']
-
-            
-#             if plate_name in plate_dict:
-                
-#                 plate_dict[plate_name] = np.vstack([plate_dict[plate_name], [longitude, latitude]])
-#             else:
-                
-#                 plate_dict[plate_name] = np.array([[longitude, latitude]])
-
-       
-#         return plate_dict
-
-#     except Exception as e:
-#         raise IOError(f"An error occurred while reading the CSV file: {str(e)}")
-# #Function 3
-# filename = './IRIS_eq_010100_112422_mag4.csv'
-    
-# def get_earthquakes(filename):
-#     try: 
-#         earthquakes = pd.read_csv(filename)
-#         return earthquakes
-#     except Exception as e: 
-#         raise I0Error (f'Error reading the earthquakes file: {e}')
-
-# #Function 4
-# def parse_earthquakes_to_np(df):
-#     lats = np.array(df["Latitude"])
-#     lons = np.array(df["Longitude"])
-#     depths = np.array(df["Depth"])
-#     magnitudes = np.array(df["Magnitude"])
-#     times_object = np.array(df["Time"])
-#     times = pd.to_datetime(times_object)
-#     return lats, lons, depths, magnitudes, times
-
 import numpy as np
 import pandas as pd
 from datetime import datetime
@@ -68,28 +8,29 @@ def get_coastlines(coasts_file):
         df = pd.read_csv(coasts_file)
         lon_coast = df.iloc[:,0]
         lat_coast = df.iloc[:,1]
+        return lon_coast, lat_coast
     except:
         raise IOError
-    return lon_coast, lat_coast
+    
 
 #Function 2
 def get_plate_boundaries(plates_files):
     try:
-        df = pd.read_csv("./all_boundaries.csv")
+        df = pd.read_csv(plates_files)
         plate = np.array(df.iloc[:, 0])
         lat = np.array(df.iloc[:, 1])
         lon = np.array(df.iloc[:, 2])
-        plates = dict()
+        pb_dict = dict()
         plate_name = ""
         for i in range(len(plate)):
-            if (plate_name != plate[i]):   
+            if plate_name != plate[i]:   
                 plate_name = plate[i]
-                plates[plate_name] = np.array([[lon[i]], [lat[i]]])
+                pb_dict[plate_name] = np.array([[lat[i]], [lon[i]]])
             else:
-                temp = plates[plate_name]
-                plates[plate_name] = np.append(temp, np.array([[lon[i]], [lat[i]]]))
-                plates[plate_name].reshape(-1, 2)
-        return plates
+                temp = pb_dict[plate_name]
+                pb_dict[plate_name] = np.append(temp, np.array([[lat[i]], [lon[i]]]), axis=1)
+        return pb_dict
+        
     except:
         raise IOError
 #Function 3
@@ -97,8 +38,8 @@ def get_earthquakes(filename):
     try: 
         earthquakes = pd.read_csv(filename)
         return earthquakes
-    except Exception as e: 
-        raise I0Error (f'Error reading the earthquakes file: {e}')
+    except:
+        raise IOError
 
 #Function 4
 def parse_earthquakes_to_np(df):
