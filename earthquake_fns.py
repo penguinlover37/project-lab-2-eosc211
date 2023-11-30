0b23dc799c6c77b03c7c586fb8f86af6613c8da7
penguinlover37 <150316172+penguinlover37@users.noreply.github.com>
2023-11-29 16:51:03 -0800

    balls

--- earthquake_fns.py
+++ earthquake_fns.py
@@ -14,6 +14,31 @@ def get_coastlines(coasts_file):
 
 #Function 2
 
+def get_plate_boundaries(boundaries_file):
+    try:
+        df = pd.read_csv(boundaries_file)
+        plate_dict = {}
+
+        
+        for index, row in df.iterrows():
+            #
+            plate_name = row['Plate Abbreviation']
+            latitude = row['Latitude']
+            longitude = row['Longitude']
+
+            
+            if plate_name in plate_dict:
+                
+                plate_dict[plate_name] = np.vstack([plate_dict[plate_name], [longitude, latitude]])
+            else:
+                
+                plate_dict[plate_name] = np.array([[longitude, latitude]])
+
+       
+        return plate_dict
+
+    except Exception as e:
+        raise IOError(f"An error occurred while reading the CSV file: {str(e)}")
 
 #Function 3
 
